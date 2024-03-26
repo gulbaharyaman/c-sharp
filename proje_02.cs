a4427e713fdc2145f7cc7c598b670c95497ba568
Gülbahar Yaman <154812348+gulbaharyaman@users.noreply.github.com>
2024-03-26 16:39:33 +0300

    üs alma programı
    girilen sayının istenilen kadar üssünü alma

---
+++ proje_02.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//girilen sayının istenilen kadarüssünü alma
+namespace ConsoleApp6
+{
+    internal class Program
+    {
+        static int us_al(int a,int b)
+        {
+            int sonuc = 1;
+            if (b == 0 && a == 0)
+                Console.Write("tanımsız");
+            else if (b == 0)
+                return sonuc;
+            else
+            {
+                for (int i = 1; i <= b; i++)
+                {
+                    sonuc = sonuc * a;
+                }
+            }
+            return sonuc; 
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("üssü alınacak sayıyı giriniz ");
+            int a=Convert.ToInt32(Console.ReadLine());
+            Console.Write("üssü giriniz ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(a+" üssü "+b+" = "+us_al(a,b));
+            Console.ReadLine();
+        }
+    }
+}
