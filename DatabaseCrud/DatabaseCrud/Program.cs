8f1ef26e13aa51cb265779b0d47c13bb9b7c60ba
Gülbahar Yaman <154812348+gulbaharyaman@users.noreply.github.com>
2025-04-22 20:50:18 +0300

    DatabaseCrud
    C# kullanarak veritabanı üzerinde CRUD işlemleri yapmak

---
+++ DatabaseCrud/DatabaseCrud/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseCrud
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Crud-->Create-Read-Update-Delete
+
+            Console.WriteLine("***** Menü Sipariş İşlem Sayfası *****");
+            Console.WriteLine();
+
+            Console.WriteLine("------------------------------------------------");
+            #region  Kategori Ekleme İşlemi         
+            //Console.WriteLine("Eklemek İstediğniz Kategori Adı: ");
+            //string categoryName=Console.ReadLine();
+
+            //SqlConnection connection = new SqlConnection("Data Source=DESKTOP-DA7KKG5\\SQLEXPRESS;initial catalog=EgitimKampiDb;integrated security=true");
+            //connection.Open();
+            //SqlCommand command = new SqlCommand("insert into TblCategory (CategoryName) values (@p1)",connection);
+            //command.Parameters.AddWithValue("@p1", categoryName);
+            //command.ExecuteNonQuery();
+            //connection.Close();
+
+            //Console.WriteLine("Kategori başarıyla eklendi");
+            #endregion
+
+            #region Ürün Ekleme İşlemi
+            //string productName;
+            //decimal productPrice;
+            ////bool productStatus;
+
+            //Console.Write("Ürün adı: ");
+            //productName = Console.ReadLine();
+            //Console.Write("Ürün fiyatı: ");
+            //productPrice=decimal.Parse(Console.ReadLine());
+
+            //SqlConnection connection=new SqlConnection("Data Source=DESKTOP-DA7KKG5\\SQLEXPRESS;initial catalog=EgitimKampiDb;integrated security=true");
+            //connection.Open();
+            //SqlCommand command = new SqlCommand("insert into TblProduct (ProductName,ProductPrice,ProductStatus) values (@productName,@productprice,@productStatus)",connection);
+            //command.Parameters.AddWithValue("@productName",productName);
+            //command.Parameters.AddWithValue("@productPrice",productPrice);
+            //command.Parameters.AddWithValue("@productStatus",true);
+            //command.ExecuteNonQuery();
+            //connection.Close();
+            //Console.Write("Ürün eklemesi başarılı");
+            #endregion
+
+            #region Ürün Listeleme İşlemi
+            //SqlConnection connection=new SqlConnection("Data Source=DESKTOP-DA7KKG5\\SQLEXPRESS;initial Catalog=EgitimKampiDb;integrated security=true");
+            //connection.Open();
+            //SqlCommand command = new SqlCommand("Select * From TblProduct",connection);
+            //SqlDataAdapter adapter = new SqlDataAdapter(command);
+            //DataTable dataTable = new DataTable();
+            //adapter.Fill(dataTable);
+
+            //foreach(DataRow row in dataTable.Rows)
+            //{
+            //    foreach(var item in row.ItemArray)
+            //    {
+            //        Console.Write(item.ToString()+" ");
+            //    }
+            //    Console.WriteLine();
+            //}
+            //connection.Close();
+            #endregion
+
+            #region Ürün Silme İşlemi
+
+            //Console.Write("Silinecek Ürün Id: ");
+            //int productId = int.Parse(Console.ReadLine());
+
+            //SqlConnection connection=new SqlConnection("Data Source=DESKTOP-DA7KKG5\\SQLEXPRESS;initial Catalog=EgitimKampiDb;integrated security=true");
+            //connection.Open();
+
+            //SqlCommand command = new SqlCommand("Delete From TblProduct Where ProductId=@productId",connection);
+            //command.Parameters.AddWithValue("@productId",productId);
+            //command.ExecuteNonQuery();
+            //connection.Close();
+            //Console.WriteLine("Silme işlemi yapıldı");
+
+            #endregion
+
+            #region Ürün Güncelleme
+
+            Console.Write("Güncellenecek ürün id: ");
+            int productId=int.Parse(Console.ReadLine());
+
+            Console.Write("Güncellenecek ürün adı: ");
+            string productName=Console.ReadLine();
+
+            Console.Write("Güncellenecek ürün fiyatı: ");
+            decimal productPrice=decimal.Parse(Console.ReadLine());
+
+            SqlConnection connection=new SqlConnection("Data Source=DESKTOP-DA7KKG5\\SQLEXPRESS;initial Catalog=EgitimKampiDb;integrated security=true");
+            connection.Open();
+            SqlCommand command=new SqlCommand("Update TblProduct Set ProductName=@productName,Productprice=@productPrice where ProductId=@productId",connection);
+            command.Parameters.AddWithValue("@productName",productName);
+            command.Parameters.AddWithValue("@productPrice", productPrice);
+            command.Parameters.AddWithValue("@productId", productId);
+            command.ExecuteNonQuery();//değişiklikleri kaydetmek için
+
+            connection.Close();
+            Console.WriteLine("Güncelleme başarılı");
+            #endregion
+            Console.Read();
+
+        }
+    }
+}
---
+++ DatabaseCrud/DatabaseCrud/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("DatabaseCrud")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DatabaseCrud")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("a593fc4d-cccb-4bd3-8cc9-e875f5437932")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
